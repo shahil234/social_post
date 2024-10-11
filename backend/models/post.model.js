0d35b159d296ee3577dc156b9b869cb4fad00015
shahil234 <shahilsomare72@gmail.com>
2024-10-11 17:51:56 +0545

    change post schema

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,23 +1,37 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema(
+  {
     image: {
-        type: String,
+      type: String,
     },
     caption: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    }
-}, {timestamps: true})
-
-
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    comments: [
+      {
+        commentText: String,
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        }
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const Post = mongoose.model("Post", postSchema);
-
 
 module.exports = Post;
